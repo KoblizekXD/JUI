150093d38c76813cbe2bef1726d79deabc730ac4
KoblizekXD <janprokupek@centrum.cz>
2023-05-03 18:12:34 +0200

    added: common libraries needed to make the project

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,18 @@ repositories {
 }
 
 dependencies {
+    // Log4J
+    implementation("org.apache.logging.log4j:log4j-core:2.20.0")
+    implementation("org.apache.logging.log4j:log4j-api:2.20.0")
+
+    // JNA
+    implementation("net.java.dev.jna:jna:5.13.0")
+    implementation("net.java.dev.jna:jna-platform:5.13.0")
+
+    // Utilities
+    implementation("com.google.guava:guava:31.1-jre")
+
+    // JUnit
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
